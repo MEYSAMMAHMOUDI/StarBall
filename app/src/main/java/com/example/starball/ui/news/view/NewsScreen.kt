a7e708b7274efffe1099cd87301933418cd90b5f
MEYSAMMAHMOUDI <42731110+MEYSAMMAHMOUDI@users.noreply.github.com>
2022-01-14 02:31:41 -0800

    Control null parameter in news request and set default image for news images

--- app/src/main/java/com/example/starball/ui/news/view/NewsScreen.kt
+++ app/src/main/java/com/example/starball/ui/news/view/NewsScreen.kt
@@ -1,5 +1,6 @@
 package com.example.starball.ui.news.view
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
@@ -12,17 +13,20 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.Image
 import androidx.compose.material.MaterialTheme
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.rememberImagePainter
 import coil.size.Scale
 import coil.transform.CircleCropTransformation
 import com.example.starball.R
 import com.example.starball.data.models.news.Article
+import com.example.starball.ui.news.NewsViewModel
 
 
 @Composable
 fun NewsListItem(article: Article) {
-
+    val context = LocalContext.current
     Card(
         modifier = Modifier
             .padding(8.dp, 4.dp)
@@ -43,7 +47,7 @@ fun NewsListItem(article: Article) {
 
                         builder = {
                             scale(Scale.FILL)
-                            placeholder(R.drawable.ic_launcher_background)
+                            placeholder(R.drawable.ic_defulte_news)
                             transformations(CircleCropTransformation())
 
                         }
@@ -54,7 +58,6 @@ fun NewsListItem(article: Article) {
                         .weight(0.5f)
                 )
 
-
                 Column(
                     verticalArrangement = Arrangement.Center,
                     modifier = Modifier
@@ -62,15 +65,18 @@ fun NewsListItem(article: Article) {
                         .fillMaxHeight()
                         .weight(0.7f)
                 ) {
+
                     Text(
-                        text = article.title,
+                        text = "Title:${article.title}",
                         style = MaterialTheme.typography.subtitle1,
                         fontWeight = FontWeight.Bold,
                         maxLines = 1,
-                        color = Color.Black
+                        color = Color.Black,
+
                     )
+
                     Text(
-                        text = article.description,
+                        text = "Description:${article.description}",
                         style = MaterialTheme.typography.caption,
                         modifier = Modifier
                             .padding(4.dp),
@@ -78,6 +84,7 @@ fun NewsListItem(article: Article) {
                         maxLines = 2,
                         color = Color.Black
                     )
+
                 }
             }
         }
